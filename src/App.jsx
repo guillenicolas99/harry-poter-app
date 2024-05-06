4a7070eded8cf93618cac4a69759f693c3fec2b6
Guillermo <guillenicolas99@gmail.com>
2024-05-06 11:58:20 -0600

    fix filter by gryffindor

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,7 @@ import './App.css'
 
 function App() {
   const [characters, setCharacters] = useState([])
-  const [charactersLength, setCharactersLength] = useState(0)
-  const [gryffindor, setGryffindor] = useState(null)
-  const [filtering, setFiltering] = useState('')
+  const [filtering, setFiltering] = useState(false)
   const [isLoading, setLoading] = useState(true)
 
   useEffect(() => {
@@ -15,27 +13,22 @@ function App() {
     .then(response => {
       const respuesta = response ? response : []
       setCharacters(respuesta)
-      setCharactersLength(respuesta.length)
     })
     .finally(() => setLoading(false))
   }, [])
 
-  const toggleHouse = () => {
-    setGryffindor(gryffindor => !gryffindor)
-    const isGryffindor = gryffindor ? 'gryffindor' : null
-    setFiltering(isGryffindor)
-  }
-
-  const filteredGryffindor = filtering != null
-    ? characters.filter(character => character.hogwartsHouse.toLowerCase().includes(filtering.toLowerCase()))
+  const filteredGryffindor = filtering
+    ? characters.filter(character => character.hogwartsHouse.toLowerCase().includes('gryffindor'))
     : characters
 
     if(isLoading) return <h2 className='loading'>LOADING...</h2>
 
+    // if(filtering.length == 0) return <h2 className='loading'>Cant Load Characters...</h2>
+
   return (
     <>
     <section className='filter-menu'>
-      <button onClick={toggleHouse}>{gryffindor ? 'No filtrar por Gryffindor' : 'Filtrar por Gryffindor'}</button>
+      <button onClick={() => setFiltering(!filtering)}>{filtering ? 'No filtrar por Gryffindor' : 'Filtrar por Gryffindor'}</button>
     </section>
       <h2>Total: {filteredGryffindor.length}</h2>
     <section className='row-characters'>
