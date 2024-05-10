8455f152f2c402dfcabd11facc1f6f7be5521808
Guillermo <guillenicolas99@gmail.com>
2024-05-10 17:07:15 -0600

    cambios menu

--- src/components/MainMenu.jsx
+++ src/components/MainMenu.jsx
@@ -1,29 +1,89 @@
 
-"use client";
 
-import { Navbar } from "flowbite-react";
-import DarkModeToggle from "./DarkModeToggle";
+'use client';
 
+import { Button, MegaMenu, Navbar } from 'flowbite-react';
 
 export default function MainMenu() {
     return (
-        <Navbar fluid className="sticky top-0 z-50 bg-black">
-            <Navbar.Brand href="#">
-                <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">Harry Potter App</span>
-            </Navbar.Brand>
-            <div className="flex md:order-2">
+        <MegaMenu className='bg-black'>
+            <div className="mx-auto flex max-w-screen-xl flex-wrap items-center justify-between p-4 md:space-x-8">
+                <Navbar.Brand href="/">
+                    <img alt="" src="/favicon.svg" className="mr-3 h-6 sm:h-9" />
+                    <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">Flowbite</span>
+                </Navbar.Brand>
                 <Navbar.Toggle />
+                <Navbar.Collapse>
+                    <Navbar.Link className='text-white' href="#">Home</Navbar.Link>
+                    <MegaMenu.Dropdown className='text-white bg-black' toggle={<>Company</>}>
+                        <ul className="grid grid-cols-3">
+                            <div className="space-y-4 p-4">
+                                <li>
+                                    <a href="#" className="hover:text-primary-600 dark:hover:text-primary-500">
+                                        About Us
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="#" className="hover:text-primary-600 dark:hover:text-primary-500">
+                                        Library
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="#" className="hover:text-primary-600 dark:hover:text-primary-500">
+                                        Resources
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="#" className="hover:text-primary-600 dark:hover:text-primary-500">
+                                        Pro Version
+                                    </a>
+                                </li>
+                            </div>
+                            <div className="space-y-4 p-4">
+                                <li>
+                                    <a href="#" className="hover:text-primary-600 dark:hover:text-primary-500">
+                                        Contact Us
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="#" className="hover:text-primary-600 dark:hover:text-primary-500">
+                                        Support Center
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="#" className="hover:text-primary-600 dark:hover:text-primary-500">
+                                        Terms
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="#" className="hover:text-primary-600 dark:hover:text-primary-500">
+                                        Blog
+                                    </a>
+                                </li>
+                            </div>
+                            <div className="space-y-4 p-4">
+                                <li>
+                                    <a href="#" className="hover:text-primary-600 dark:hover:text-primary-500">
+                                        Newsletter
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="#" className="hover:text-primary-600 dark:hover:text-primary-500">
+                                        Playground
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="#" className="hover:text-primary-600 dark:hover:text-primary-500">
+                                        License
+                                    </a>
+                                </li>
+                            </div>
+                        </ul>
+                    </MegaMenu.Dropdown>
+                    <Navbar.Link className='text-white' href="#">Team</Navbar.Link>
+                </Navbar.Collapse>
             </div>
-            <Navbar.Collapse>
-                <Navbar.Link className="text-white text-2xl" href="#" active>
-                Home
-                </Navbar.Link>
-                <Navbar.Link className="text-white text-2xl" href="#">About</Navbar.Link>
-                <Navbar.Link className="text-white text-2xl" href="#">Services</Navbar.Link>
-                <Navbar.Link className="text-white text-2xl" href="#">Pricing</Navbar.Link>
-                <Navbar.Link className="text-white text-2xl" href="#">Contact</Navbar.Link>
-            </Navbar.Collapse>
-            <DarkModeToggle />
-        </Navbar>
+        </MegaMenu>
     );
 }
+
